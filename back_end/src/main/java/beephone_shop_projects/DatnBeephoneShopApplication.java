62213ad0fe95189e657e6e311f768a258c5a057f
codingaslife <hatran.devs@gmail.com>
2023-08-24 16:57:52 +0700

    add Bean ModelMapper

--- back_end/src/main/java/beephone_shop_projects/DatnBeephoneShopApplication.java
+++ back_end/src/main/java/beephone_shop_projects/DatnBeephoneShopApplication.java
@@ -1,13 +1,20 @@
 package beephone_shop_projects;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DatnBeephoneShopApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DatnBeephoneShopApplication.class, args);
-    }
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(DatnBeephoneShopApplication.class, args);
+  }
 
 }
