dedbfa3cd8fea5c2f07619fd64131ea69cd4f20a
codingaslife <hatran.devs@gmail.com>
2023-08-24 16:58:11 +0700

    update entity LichSuHoaDon

--- back_end/src/main/java/beephone_shop_projects/entity/LichSuHoaDon.java
+++ back_end/src/main/java/beephone_shop_projects/entity/LichSuHoaDon.java
@@ -24,7 +24,7 @@ public class LichSuHoaDon extends PrimaryEntity implements IsIdentified {
 
     private Integer loaiThaoTac;
 
-    private String mota;
+    private String moTa;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "hoa_don_id")
