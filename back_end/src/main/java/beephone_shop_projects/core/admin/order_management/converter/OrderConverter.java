9c1c07574b9461c1e405b68c86fb3857ea8b0032
codingaslife <hatran.devs@gmail.com>
2023-08-24 16:55:41 +0700

    add converter

---
+++ back_end/src/main/java/beephone_shop_projects/core/admin/order_management/converter/OrderConverter.java
@@ -0,0 +1,9 @@
+package beephone_shop_projects.core.admin.order_management.converter;
+
+import beephone_shop_projects.core.admin.order_management.dto.OrderDto;
+import beephone_shop_projects.entity.HoaDon;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderConverter extends AbstractConverter<OrderDto, HoaDon> {
+}
