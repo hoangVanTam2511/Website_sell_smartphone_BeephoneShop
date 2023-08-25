07be208e786550cdd37fdf302c1a04ed29318009
codingaslife <hatran.devs@gmail.com>
2023-08-25 15:17:32 +0700

    update constants

--- back_end/src/main/java/beephone_shop_projects/core/admin/order_management/constant/SystemConstant.java
+++
@@ -1,6 +0,0 @@
-package beephone_shop_projects.core.admin.order_management.constant;
-
-public class SystemConstant {
-  public static final String PREFIX_CODE_ORDER = "HD";
-  public static final String SUFFIX_CODE = "000000";
-}
