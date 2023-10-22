aefdd702615c02ce2b592dc997048dbee31b2a21
DESKTOP-T6LJGMF\hoant <hoanguyenthi210303@gmail.com>
2023-10-22 13:06:56 +0700

    [fix] CR Display / SimCard / dpg

--- back_end/src/main/java/beephone_shop_projects/repository/IDoPhanGiaiRepository.java
+++ back_end/src/main/java/beephone_shop_projects/repository/IDoPhanGiaiRepository.java
@@ -4,4 +4,5 @@ import beephone_shop_projects.entity.DoPhanGiaiManHinh;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IDoPhanGiai extends JpaRepository<DoPhanGiaiManHinh,String> {
+    DoPhanGiaiManHinh findByMa(String ma);
 }
